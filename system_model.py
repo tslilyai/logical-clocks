48b530dc6a63f88f52cfb83ef262eb600013e743
Lily Tsai <tslilyai@gmail.com>
2016-03-02 17:39:24 -0500

    create process class

--- system_model.py
+++ system_model.py
@@ -1,46 +1,7 @@
 import sys
 import time
 import random 
-import thread
-
-'''
-If there is a message in the message queue for the machine:
-    take one message off the queue
-    update the local logical clock
-    log: 
-        received a message
-        the global time (gotten from the system)
-        the length of the message queue
-        the logical clock time.
-
-If there is no message in the queue,generate a random number in the range of 1-10:
-    1: send to one of the other machines a message that is the local logical clock time
-    2: send to the other virtual machine a message that is the local logical clock time.
-    3: send to both of the other virtual machines a message that is the logical clock time
-    Other than 1-3: treat the cycle as an internal event
-    *For all events:
-        Update the local logical clock
-        Log the event, the system time, and the logical clock value
-'''
-def do_work(f):
-    event = random.randint(1,10)
-    f.write("Event: %s\tGlobal Time: %s\tMsgQueue Length: %d\tLC Time: %s\n" % 
-            (event_tpe, global_time, queue_len, lc_time))
-
-'''
-Initialization:
-    Initialize network queues
-    Initialize logs
-'''
-def init_process(proc_num, sockets, clock_speed):
-    # initialize stuff 
-    try:
-        with open("%d-events.log" % proc_num, 'w') as f:
-            while(1):
-                sleep(clock_speed)
-                do_work(f)
-    except KeyboardInterrupt:
-        sys.exit()
+from process import Process
 
 '''
     Start 3 processes (threads?)
@@ -49,10 +10,12 @@ def init_process(proc_num, sockets, clock_speed):
         Random number between 1 and 6
 '''
 def main():
+    # TODO
     #sockets = 
     for i in range(3):
         clock_speed = random.randint(1,6)
-        init_process(i, sockets, clock_speed)
+        p = Process(i, sockets, clock_speed)
+        p.run_process()
 
 if __name__ == "__main__":
     main()
